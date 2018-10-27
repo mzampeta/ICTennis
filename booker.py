391a62e6ee729c00fdb26d215ed62fcd18c927e7
mzampeta <mzampeta@gmail.com>
2018-10-27 01:44:27 +0100

    First Commit

---
+++ booker.py
@@ -0,0 +1,20 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+my_username = os.environ.get('IC_USER')
+my_password = os.environ.get('IC_PASS')
+
+with requests.Session() as c:
+    url = 'https://union.ic.ac.uk/acc/tennis/booking/login'
+    c.get(url)
+    payload = {'cid_user': my_username, 'cid_pass': my_password}
+    c.post(url, data=payload)
+    page = c.get('https://union.ic.ac.uk/acc/tennis/booking')
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+    #num = soup.find('a', attrs={'class':'btn disabled'})
+    links = soup.find_all('a', attrs={'class':'btn disabled'}, href=True)
+    for l in links:
+        print (l['href'])
+
